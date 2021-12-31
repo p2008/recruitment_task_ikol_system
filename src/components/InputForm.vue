111477423fff8b557986355f7b2bb43d467237a3
pawel.podolski <pawel.1.podolski@uj.edu.pl>
2021-12-31 17:09:16 +0100

    Refactor of InputForm.vue

--- src/components/InputForm.vue
+++ src/components/InputForm.vue
@@ -5,76 +5,33 @@
     lazy-validation
     @submit.prevent="submit"
   >
-    <div>
-      <label>
-        Start point
-      </label>
 
-      <v-text-field
-        v-model="geoPoints.start.latitude"
-        :rules="pointRules.latitude"
-        label="Start point latitude"
-        required
-        type="number"
-      />
+    <base-geo-points-input
+      v-model="geoPoints.start"
+      label="Start point"
+    />
 
-      <v-text-field
-        v-model="geoPoints.start.longitude"
-        :rules="pointRules.longitude"
-        label="Start point longitude"
-        required
-        type="number"
-      />
-    </div>
+    <base-geo-points-input
+      v-model="geoPoints.end"
+      label="End point"
+    />
 
-    <div>
-      <label>
-        End point
-      </label>
-
-      <v-text-field
-        v-model="geoPoints.end.latitude"
-        :rules="pointRules.latitude"
-        label="End point latitude"
-        required
-        type="number"
-      />
-
-      <v-text-field
-        v-model="geoPoints.end.longitude"
-        :rules="pointRules.longitude"
-        label="End point longitude"
-        required
-        type="number"
-      />
-    </div>
-
-    <div>
-      <v-btn
-        :disabled="!valid"
-        class="mr-4"
-        color="success"
-        type="submit"
-        @click="validate"
-      >
-        Submit
-      </v-btn>
-
-      <v-btn
-        class="mr-4"
-        color="error"
-        @click="clearForm">
-        Clear form
-      </v-btn>
-    </div>
+    <input-form-buttons
+      @submit="validate"
+      @clear="clearForm"
+      :valid="valid"
+    />
   </v-form>
 
 </template>
 
 <script>
+import BaseGeoPointsInput from '@/components/BaseGeoPointsInput';
+import InputFormButtons from '@/components/InputFormButtons';
+
 export default {
   name: 'InputForm',
-
+  components: { InputFormButtons, BaseGeoPointsInput },
   data: () => ({
     valid: true,
     geoPoints: {
@@ -86,16 +43,6 @@ export default {
         latitude: '',
         longitude: '',
       },
-    },
-    pointRules: {
-      latitude: [
-        (v) => !!v || 'field is required',
-        (v) => (v && v >= -90 && v <= 90) || 'field must be in range (-90, 90)',
-      ],
-      longitude: [
-        (v) => !!v || 'field is required',
-        (v) => (v && v >= -180 && v <= 180) || 'field must be in range (-180, 180)',
-      ],
     },
   }),
 
---
+++ src/components/InputFormButtons.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-btn
+      :disabled="!$attrs.valid"
+      class="mr-4"
+      color="success"
+      type="submit"
+      @click="$listeners.submit"
+    >
+      Submit
+    </v-btn>
+
+    <v-btn
+      class="mr-4"
+      color="error"
+      @click="$listeners.clear">
+      Clear form
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputFormButtons',
+};
+</script>
+
+<style scoped>
+
+</style>
