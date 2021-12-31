41163e33f18d3fbbad087807a97c18c3a2099de3
pawel.podolski <pawel.1.podolski@uj.edu.pl>
2021-12-31 15:10:13 +0100

    Add basic input form functionality

---
+++ src/components/InputForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <form @submit.prevent="submit">
+    <v-text-field
+      v-model="geoPoints.start"
+      label="Start point"
+      required
+    />
+
+    <v-text-field
+      v-model="geoPoints.end"
+      label="End point"
+      required
+    />
+
+    <v-btn
+      class="mr-4"
+      type="submit"
+    >
+      submit
+    </v-btn>
+
+    <v-btn
+      @click="clear">
+      clear
+    </v-btn>
+  </form>
+
+</template>
+
+<script>
+export default {
+  name: 'InputForm',
+
+  data: () => ({
+    geoPoints: {
+      start: '',
+      end: '',
+    },
+  }),
+
+  methods: {
+    submit() {
+      this.$emit('form-submit', this.geoPoints);
+    },
+    clear() {
+      this.geoPoints = {};
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
